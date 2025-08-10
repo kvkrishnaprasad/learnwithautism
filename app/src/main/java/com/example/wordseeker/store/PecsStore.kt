32dfe086827c7d423c2e899d219a8df27a4ada31
venkat <venkat.sushanth.2011@gmail.com>
2025-08-10 19:59:24 +0100

    New

--- app/src/main/java/com/example/wordseeker/store/PecsStore.kt
+++ app/src/main/java/com/example/wordseeker/store/PecsStore.kt
@@ -79,24 +79,24 @@ object PecsStore {
         addItem("People", Pecs("Anna", R.drawable.apple, R.raw.one00001))
         addItem("People", Pecs("Navomi Maam", R.drawable.apple, R.raw.one00001))
 
-        addItem("Food", Pecs("Vegetables", R.drawable.apple, R.raw.one00001))
-        addItem("Food", Pecs("Fruits", R.drawable.fruit_infused, R.raw.one00001))
+        addItem("Food", Pecs("Vegetables", R.drawable.vegetables, R.raw.vegeta01))
+        addItem("Food", Pecs("Fruits", R.drawable.fruit_infused, R.raw.fruit001))
 
-        addItem("Snacks", Pecs("Chocolate", R.drawable.apple, R.raw.one00001))
-        addItem("Snacks", Pecs("French Fries", R.drawable.apple, R.raw.one00001))
-        addItem("Snacks", Pecs("Candy", R.drawable.apple, R.raw.one00001))
-        addItem("Snacks", Pecs("Popcorn", R.drawable.apple, R.raw.one00001))
+        addItem("Snacks", Pecs("Chocolate", R.drawable.chocolate, R.raw.chocol11))
+        addItem("Snacks", Pecs("French Fries", R.drawable.large_fries, R.raw.french_fries))
+        addItem("Snacks", Pecs("Candy", R.drawable.candy, R.raw.candy001))
+        addItem("Snacks", Pecs("Popcorn", R.drawable.popcorn, R.raw.popcor01))
 
-        addItem("Places", Pecs("Mcdonald's", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("Home", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("School", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("Nana Car", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("Amma Car", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("Office", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("Shop", R.drawable.apple, R.raw.one00001))
-        addItem("Places", Pecs("Gym", R.drawable.apple, R.raw.one00001))
+        addItem("Places", Pecs("Mcdonald's", R.drawable.mcdonalds, R.raw.mcdonalds))
+        addItem("Places", Pecs("Home", R.drawable.clayfarm_drive, R.raw.home0001))
+        addItem("Places", Pecs("School", R.drawable.fawcett_primary_school, R.raw.school01))
+        addItem("Places", Pecs("Nana Car", R.drawable.toyota_yaris_cross, R.raw.car00001))
+        addItem("Places", Pecs("Amma Car", R.drawable.audi_q4_etron, R.raw.car00001))
+        addItem("Places", Pecs("Office", R.drawable.arm_office, R.raw.office01))
+        addItem("Places", Pecs("Waitrose", R.drawable.waitrose, R.raw.shop0001))
+        addItem("Places", Pecs("Gym", R.drawable.david_lloyds, R.raw.gym00001))
 
-        addItem("Items", Pecs("Lego", R.drawable.apple, R.raw.one00001))
+        addItem("Items", Pecs("Lego", R.drawable.lego, R.raw.lego))
 
     }
 
